7f7513a06f03d6eed19bb77457f489dc4f560c72
DarinaStegailo221RDB186 <125799578+DarinaStegailo221RDB186@users.noreply.github.com>
2023-02-19 20:00:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-    print(misimatch)
+    print(mismatch)
 
 
 if __name__ == "__main__":
