b1b45e884cd55885aa1e8e68e13a38b93e9d1b38
DarinaStegailo221RDB186 <125799578+DarinaStegailo221RDB186@users.noreply.github.com>
2023-02-20 01:25:02 +0200

    Update main.py
    Darina Stegailo 221RDB186

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i + 1))
+            b = Bracket (i, next)
+            opening_brackets_stack.append(b)
 
         if next in ")]}":
             if not opening_brackets_stack:
                 return i + 1
-            top = opening_brackets_stack.pop()
-            if not are_matching(top.char, next):
+            if not are_matching(opening_brackets_stack.pop.char, next):
                 return i + 1
 
     if opening_brackets_stack:
