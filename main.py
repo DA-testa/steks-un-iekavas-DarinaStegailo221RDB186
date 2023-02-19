e43aaaa3a68cbc146379fd11393ab57be37c162c
DarinaStegailo221RDB186 <125799578+DarinaStegailo221RDB186@users.noreply.github.com>
2023-02-19 23:03:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,29 +14,42 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
             b = Bracket (i, next)
             opening_brackets_stack.append(b)
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
             if not opening_brackets_stack:
                 return i + 1
-            p = opening_brackets_stack.pop()
-            if (p.char == '('and next !=')') or (p.char == '['and next !=']') or (p.char == '{'and next !='}'):
-                return i+1       
+            obs = opening_brackets_stack[-1]
+            if not are_matching(obs.char, next):
+                return i+1
+            opening_brackets_stack.pop()
+
         if opening_brackets_stack:
-            return opening_brackets_stack[0].position + 1
-        return "Success"
+            return obs.position
+        else:
+            return "Success"
 
 
 def main():
+     # Printing answer, write your code here
+    i = input("F" or "I")
+    if "F" in i:
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    print(mismatch)
-
-
+    if mismatch == "Success":
+        print("Success")
+    else:
+        print (mismatch)
+    
+    if "F" in i:
+    text = input()
+    mismatch = find_mismatch(text)
+    if mismatch == "Success":
+        print("Success")
+    else:
+        print (mismatch)
+    
 if __name__ == "__main__":
     main()
